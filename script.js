c3647dfb7910a40cad8ca47337f8c6180b7d0c53
aks2023 <aks2023@hotmail.com>
2024-01-17 13:27:18 +0530

    Number Checking

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  if (guess === 5) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else {
+    document.querySelector('.message').textContent = 'Incorrect Number';
+  }
+});
